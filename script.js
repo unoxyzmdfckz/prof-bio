84942fb0cfc269f280598a5e400cc9fd34e9dd48
gra gra pow <155625311+unoxyzmdfckz@users.noreply.github.com>
2024-01-04 18:26:19 +0800

    uno

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
-let data = ["hello, world!", "i love skidding", "i love boobs"];
+let data = ["print('hello, world!')", "i love skidding", "i love boobs"];
 let len = 0;
 let speed=150;
-for (let i = 0; i < data.length; ++i) len += 2*data[i].length;
+for (let i = 0; i < data.length; ++i) len += 3*data[i].length;
 len += 4*data.length;
 function type() {
     let p=0;
@@ -60,28 +60,60 @@ bitcoinDiv.addEventListener('click', function() {
     document.execCommand('copy');
     document.body.removeChild(copyText);
 
-    // Ändere den Text zu "Copied!" nach dem Kopieren
     tooltip.innerHTML = 'Copied!';
     tooltip.style.display = 'inline-block';
 
-    // Entferne die Klasse 'clicked' und setze den Text zurück nach einigen Sekunden
     setTimeout(function() {
       tooltip.style.display = 'none';
-      tooltip.innerHTML = 'Copy Adress'; // Setze den Text zurück
+      tooltip.innerHTML = 'Copy Adress'; 
       isCopying = false;
     }, 1500);
   }
 });
 
-// Zeige die Tooltip-Box beim Überfahren mit dem Mauszeiger
 bitcoinDiv.addEventListener('mouseover', function() {
   tooltip.style.display = 'inline-block';
 });
 
-// Verstecke die Tooltip-Box nach dem Verlassen mit dem Mauszeiger
 bitcoinDiv.addEventListener('mouseout', function() {
   tooltip.style.display = 'none';
 });
+
+var ltcDiv = document.getElementById('bitcoinDiv');
+var tooltip = document.getElementById('tooltip');
+var bitcoinAddress = bitcoinDiv.getAttribute('data-address');
+var isCopying = false;
+
+bitcoinDiv.addEventListener('click', function() {
+  if (!isCopying) {
+    isCopying = true;
+    
+    var copyText = document.createElement('textarea');
+    copyText.value = bitcoinAddress;
+    document.body.appendChild(copyText);
+    copyText.select();
+    document.execCommand('copy');
+    document.body.removeChild(copyText);
+
+    tooltip.innerHTML = 'Copied!';
+    tooltip.style.display = 'inline-block';
+
+    setTimeout(function() {
+      tooltip.style.display = 'none';
+      tooltip.innerHTML = 'Copy Adress'; 
+      isCopying = false;
+    }, 1500);
+  }
+});
+
+bitcoinDiv.addEventListener('mouseover', function() {
+  tooltip.style.display = 'inline-block';
+});
+
+bitcoinDiv.addEventListener('mouseout', function() {
+  tooltip.style.display = 'none';
+});
+
 
 function startSite() {
   var introContainer = document.getElementById('introContainer');
@@ -92,7 +124,6 @@ function startSite() {
       introContainer.style.display = 'none';
       mainContent.style.display = 'block';
 
-      // Start background music
       backgroundMusic.play().catch(function (error) {
           console.error('Audio autoplay failed:', error);
       });
